660ed3163805c5f6b4c98086fdc0bb7244e17f4d
nexuster <coralpinklmao@outlook.com>
2025-02-07 14:34:29 -0500

    men

---
+++ game.js
@@ -0,0 +1,48 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('logo', 'https://labs.phaser.io/assets/sprites/phaser3-logo.png');
+    this.load.image('red', 'https://labs.phaser.io/assets/particles/red.png');
+}
+
+function create() {
+    this.add.image(400, 300, 'sky');
+
+    const particles = this.add.particles('red');
+    const emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    const logo = this.physics.add.image(400, 100, 'logo');
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+}
+
+
+function update() {
+    // insert game logic
+}
